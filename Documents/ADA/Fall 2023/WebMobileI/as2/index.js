b8bbde73885c7cef1864b803f926ac38a88a115e
ilpalazz0 <shasanov0305@gmail.com>
2023-12-03 18:35:07 +0400

    uwu

--- Documents/ADA/Fall 2023/WebMobileI/as2/index.js
+++ Documents/ADA/Fall 2023/WebMobileI/as2/index.js
@@ -5,22 +5,71 @@ fetch("https://dummyjson.com/products")
     //.then(data => console.log(data.products))
     .then(data=> {
         data.products.forEach(products => {
+     
+            a = products.price;
+            b = products.discountPercentage;
             const markup = 
-            `<tr onclick="window.open('info.html', '_blank')">
+            `<div id="product" class="${products.category}" onclick=window.open('info.html?id=${products.id}'),'_blank'>
 
-            <td><img src = '${products.images[0]}' alt='Image is currently unavailable'></td>
-            <td>${products.title}</td>
-            <td>${products.price}$</td>
-            <td>${products.discountPercentage}%</td>
-            <td>${products.stock}</td>
-            <td>${products.category}</td>
+            <p id="thumbnail"><img src = '${products.thumbnail}' alt='Image of ${products.title}'></p>
+            <p id="title">${products.title}</p>
+            <p id="price">${products.price}$</p>
+            <p id="discount">${Mult(a,b)}$</p>
+            <p id="stock">${products.stock} left in stock</p>
+            <p id="category" class="${products.category}">Category: ${products.category}</p>
 
-            </tr>`;
+            </div>`;
 
-            document.querySelector('#data').insertAdjacentHTML('beforeend', markup);
+            document.querySelector('#info').insertAdjacentHTML('beforeend', markup);
+
         });
     })
-    .catch(error => console.log(error));
+.catch(error => console.log(error));
 
+//Find discounted price
+const Mult = (a,b) => {
+    b = (100-b)/100;
+    return (a*b).toFixed(2);
+}
 
+const Filter = (cat) => {
 
+    let obj = document.querySelectorAll("#product")
+    if(cat != 'all') {
+
+    obj.forEach(hide => {
+                
+        if(hide.className != cat) {hide.style.display="none";}
+        else hide.style.display="inline-block";
+        }
+    )
+}    
+    else {
+
+        obj.forEach(show => {
+                show.style.display="inline-block";
+            }
+        )
+    }
+}
+
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('input');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("info");
+    li = ul.getElementsByTagName('p');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    let obj = document.querySelectorAll("#product")
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }

101b7bc9f98c37975d8df9f6b46cd3aa78e0f740
ilpalazz0 <shasanov0305@gmail.com>
2023-12-04 21:48:00 +0400

    fin

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-fetch("https://dummyjson.com/products")
+//Fetch data and print properties as <p> tags inside a <div>
+fetch(`https://dummyjson.com/products?limit=100`)
     .then(res => {
     return res.json();
     })
-    //.then(data => console.log(data.products))
     .then(data=> {
         data.products.forEach(products => {
-     
+
             a = products.price;
             b = products.discountPercentage;
+
             const profiles = 
+
             `<div id="product" class="${products.category}" onclick=window.open('info.html?id=${products.id}'),'_blank'>
 
             <p id="thumbnail"><img src = '${products.thumbnail}' alt='Image of ${products.title}'></p>
@@ -25,10 +27,24 @@ fetch("https://dummyjson.com/products")
             document.querySelector('#product-list').insertAdjacentHTML('beforeend', list);
             document.querySelector('#info').insertAdjacentHTML('beforeend', profiles);
 
-
-
         });
     })
+.catch(error => console.log(error));
+
+fetch("https://dummyjson.com/products/categories")
+    .then(res => res.json())
+    .then(data => {
+            let i=0;
+            data.forEach(category => {
+                optionValue = data[i];
+                const filters = `<option value='${data[i]}'>${optionValue[0].toUpperCase()+optionValue.slice(1)}</option>`
+    
+                document.querySelector('#select-options').insertAdjacentHTML('beforeend', filters);
+                
+                i++;
+
+            });
+        })
 .catch(error => console.log(error));
 
 //Find discounted price
@@ -37,18 +53,19 @@ const Mult = (a,b) => {
     return (a*b).toFixed(2);
 }
 
+// Filter function
 const Filter = (cat) => {
 
     let obj = document.querySelectorAll("#product")
     if(cat != 'all') {
 
-    obj.forEach(hide => {
-                
-        if(hide.className != cat) {hide.style.display="none";}
-        else hide.style.display="inline-block";
-        }
-    )
-}    
+        obj.forEach(hide => {
+                    
+            if(hide.className != cat) {hide.style.display="none";}
+            else hide.style.display="inline-block";
+            }
+        )
+    }       
     else {
 
         obj.forEach(show => {
@@ -58,23 +75,22 @@ const Filter = (cat) => {
     }
 }
 
-const myFunction = () => {
+// Search function
+const handleSearch = () => {
     
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('input');
     filter = input.value.toUpperCase();
     ul = document.getElementById("product-list");
     li = ul.getElementsByTagName('li');
-  
-
     for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1 && filter != '') {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1 && filter != '') {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
     }
-  }
+}
 
--- info.js
+++ info.js
@@ -1,28 +1,43 @@
 let id_location = window.location.href;
 let id = id_location.substring(id_location.indexOf("=")+1, id_location.length);
-//console.log(id);
 
 link=`https://dummyjson.com/product/${id}`
 
 fetch(link)
     .then(res => res.json())
-
+    //.then(data => console.log(data))
     .then(data => {
-            const markup = 
-            `<div id="info",'_blank'>
-
-            <p id="thumbnail"><img src = '${data.thumbnail}' alt='Image is currently unavailable'></p>
+            a = data.price;
+            b = data.discountPercentage;
+            const profile = 
+            `
+            <p id="thumbnail"><img class="thumbnail" src = '${data.thumbnail}' alt='Image is currently unavailable'></p>
+            <div id="item-details">
             <p id="title">${data.title}</p>
             <p id="price">${data.price}$</p>
-            <p id="discount">${data.discountPercentage}%</p>
-            <p id="stock">${data.stock}</p>
-            <p id="category">${data.category}</p>
+            <p id="discount">${Mult(a,b)}$</p>
+            <p id="rating">Rating: ${data.rating}<img id="star" src="https://cdn-icons-png.flaticon.com/512/118/118669.png"></p>
+            <p id="stock">${data.stock} left in stock</p>
+            <p id="category">Category: ${data.category}</p>
+            <p id="brand">Brand: ${data.brand}</p>
+            </div>
+            <p id="description">About this item: <br>${data.description}</p>`;
+            let i = 0;
+            data.images.forEach(images => {
+                const photo = `<div id="gallery"><img class="gallery-image" src = '${data.images[i]}' alt='Image of ${data.title}'></p></div>`;
+                document.querySelector('#images').insertAdjacentHTML('beforeend', photo);
+                i++;
+            })
 
+            document.querySelector('#info').insertAdjacentHTML('beforeend', profile);
 
-
-            </div>`;
-
-            document.querySelector('#info').insertAdjacentHTML('beforeend', markup);
 
         })
 .catch(error => console.log(error));
+
+//Find discounted price
+
+const Mult = (a,b) => {
+    b = (100-b)/100;
+    return (a*b).toFixed(2);
+}
